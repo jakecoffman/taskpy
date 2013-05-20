f1784bc3cf06527ea68555ae10b342d54a7b1070
Michael Cooper <mic159@gmail.com>
2013-05-20 23:02:56 +1000

    Remove unused files

--- taskpy/views/tasks.py
+++ taskpy/views/tasks.py
@@ -83,19 +83,3 @@ class TasksView(BaseModelView):
 		flask.g.configuration.add(model)
 		flask.g.configuration.save()
 		return True
-
-class Tasks(MethodView):
-
-    # Get list of tasks, or get one task
-    def get(self):
-        return flask.render_template('tasks.html', tasks=flask.g.tasks.get())
-
-    # Add new task
-    def post(self):
-        form = flask.request.form
-        name = form['name']
-        script = ''
-        if 'script' in form:
-            script = form['script']
-        flask.g.tasks.save_task(name, script)
-        return '/tasks/'
