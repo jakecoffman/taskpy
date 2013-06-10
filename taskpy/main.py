ff701cc01b99018bbbfed525cf0fdd97090541e3
Michael Cooper <mic159@gmail.com>
2013-06-11 01:01:38 +1000

    Create base directory if it does not exist

--- taskpy/main.py
+++ taskpy/main.py
@@ -9,14 +9,22 @@ from taskpy.models import db
 from taskpy.worker import celery
 
 def make_app():
+	# Setup flask and settings
 	app = flask.Flask(__name__)
 	app.secret_key = 'taskpy123'
 	app.config['TASKPY_BASE'] = os.path.expanduser(os.path.join('~', '.taskpy'))
 	app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///%s' % os.path.join(app.config['TASKPY_BASE'], 'main.db')
+
+	# Make sure TASKPY_BASE exists
+	if not os.path.exists(app.config['TASKPY_BASE']):
+		os.mkdir(app.config['TASKPY_BASE'])
+
+	# Init database
 	db.init_app(app)
 	db.app = app
 	db.create_all()
 
+	# Add views
 	index_view = taskpy.views.JobsView(name="Jobs", endpoint="jobs", url='/')
 	admin_app = admin.Admin(app, name='Taskpy', index_view=index_view, base_template='admin_base.html')
 	admin_app.add_view(taskpy.views.TasksView(name="Tasks", endpoint="tasks"))
@@ -37,9 +45,13 @@ def main():
 	app.run(debug=True, host=args.host)
 
 def celery_main():
-	app = make_app()
+	# Setup argv to add the 'worker' command for celery
 	argv = sys.argv
 	argv.insert(1, 'worker')
+
+	# Run celery under the flask context
+	# to allow it to access the database
+	app = make_app()
 	with app.app_context():
 		celery.start(argv)
 
